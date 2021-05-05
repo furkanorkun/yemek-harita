20468c84861ec353721e95015e494fb194fd6e9d
furkanorkun <furkanorkn@gmail.com>
2021-05-05 16:50:55 +0300

    home sayfası yapıldı

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets/fonts/'],
+};
+
+/* 1.Custom font eklemek için react-native.config.js dosyası oluşturuldu
+   2.İçine yukarıdaki kodlar eklendi assets ile fontların yolu gösterildi
+   3.yarn react-native link terminalde yazıldı */
